60885019bff0aac2cd3466d34d3a3a79c635f997
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-05-22 16:41:19 -0300

    Create Animal.java

---
+++ src/lista4/classes/Animal.java
@@ -0,0 +1,14 @@
+package lista4.classes;
+
+public class Animal {
+	private String tipo;
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+}
