6b8fd4123a55e58dfebcf0c807b32b0d94070966
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-05-22 16:44:06 -0300

    Create ContaBancariaMain.java

---
+++ src/lista4/test/ContaBancariaMain.java
@@ -0,0 +1,19 @@
+package lista4.test;
+
+import lista4.classes.ContaBancaria;
+
+public class ContaBancariaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ContaBancaria conta1 = new ContaBancaria();
+		ContaBancaria conta2 = new ContaBancaria();
+		
+		conta1.setNomeTitular("Lorena Agatha Cavalcanti");
+		conta1.setSaldo(549.67);
+		conta2.setNomeTitular("Marina Esther Emily Aragão");
+		conta2.setSaldo(-23.67);
+	}
+
+}
