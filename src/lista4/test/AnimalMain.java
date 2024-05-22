69c9dacfbbdf2bae8115a2bb5afd030593630714
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-05-22 16:43:47 -0300

    Create AnimalMain.java

---
+++ src/lista4/test/AnimalMain.java
@@ -0,0 +1,17 @@
+package lista4.test;
+
+import lista4.classes.Animal;
+
+public class AnimalMain {
+	
+	public static void main(String[] args) {
+		Animal A1 = new Animal();
+		Animal A2 = new Animal();
+		Animal A3 = new Animal();
+		
+		A1.setTipo("Aquático");
+		A2.setTipo("Terrestre");
+		A3.setTipo("Marinho");
+	}
+	
+}
