e94f36d0d339d4f696a4a8aa1552301b8ce53ba1
João Elias Ferraz Santana <65469472+eng-joaoelias@users.noreply.github.com>
2024-05-22 16:41:49 -0300

    Create ContaBancaria.java

---
+++ src/lista4/classes/ContaBancaria.java
@@ -0,0 +1,21 @@
+package lista4.classes;
+
+public class ContaBancaria {
+	private double saldo;
+	private String nomeTitular;
+	
+	public double getSaldo() {
+		return saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public String getNomeTitular() {
+		return nomeTitular;
+	}
+	public void setNomeTitular(String nomeTitular) {
+		this.nomeTitular = nomeTitular;
+	}
+	
+	
+}
